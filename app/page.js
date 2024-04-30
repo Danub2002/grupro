419d1f1a2fbfb059f8a6e75e0caa956489b20dc2
Danilo Andrade <andrade20danilo@gmail.com>
2024-04-29 23:09:46 -0300

    feat: Cards da Landing Page ajustados e protótipo de menu sanduíche

--- app/page.js
+++ app/page.js
@@ -1,54 +1,69 @@
-import goat from '../public/goat.svg';
-import book from "../public/book.svg";
-import star from "../public/star.svg";
-import pencil from "../public/pencil.svg";
-import Card from '@/components/Card';
+import goat from '../public/goat.svg'
+import book from '../public/book.svg'
+import star from '../public/star.svg'
+import pencil from '../public/pencil.svg'
+import Card from '@/components/Card'
+import Image from 'next/image'
 
-import Image from 'next/image';
 export default function Home() {
-  const cards = [
-    {
-      icon:pencil,
-      name:"Treinamentos",
-      content: "Quer ser um programador de sucesso? Podemos apontar o caminho!",
-    },
-    {
-      icon:book,
-      name:"Histórico",
-      content:"Confira os resultados do GRUPRO em eventos locais, regionais, nacionais e internacionais!"
-    },
-    {
-      icon:star,
-      name:"Hall da Fama",
-      content:"Confiram os alunos do GRUPRO que já fizeram história representando a UFBA!"
-    }
-  ];
+    const cards = [
+        {
+            icon: pencil,
+            name: 'Treinamentos',
+            content: `Quer ser um programador de sucesso? Podemos apontar o caminho!`,
+            href: '/'
+        },
+        {
+            icon: book,
+            name: 'Histórico',
+            content:
+                'Confira os resultados do GRUPRO em eventos locais, regionais, nacionais e internacionais!',
+            href:'/'
+        },
+        {
+            icon: star,
+            name: 'Hall da Fama',
+            content:
+                'Confiram os alunos do GRUPRO que já fizeram história representando a UFBA!',
+            href:'/'
+        }
+    ]
 
-  return (
-   <>
-    <div className="relative">
-        <Image
-          objectFit="cover"
-          src={goat}
-          width={1935}
-          height={1451}
-          alt="goat"
-        />
+    return (
+        <>
+            <div className="relative">
+                <Image
+                    objectFit="cover"
+                    src={goat}
+                    width={1935}
+                    height={1451}
+                    alt="goat"
+                />
 
-        <div className="flex flex-col items-start gap-5 absolute top-20 left-9 p-4 text-xl font-bold text-white">
-            <p>GRUpo de PROgramação <br/> Venha se aprimorar e se divertir com a gente</p>
+                <div className="flex flex-col items-start gap-5 absolute top-20 left-9 p-4 text-xl font-bold text-white">
+                    <p>
+                        GRUpo de PROgramação <br /> Venha se aprimorar e se
+                        divertir com a gente
+                    </p>
 
-            <button className='bg-amber-400 text-black rounded p-2 text-sm'>Fale Conosco</button>
-        </div>
-      </div>
+                    <button className="bg-amber-400 text-black rounded p-2 text-sm">
+                        Fale Conosco
+                    </button>
+                </div>
+            </div>
 
-      <div className='flex justify-evenly m-28 gap-20 items-center'>
-        {cards.map((cardData,index) => (
-          <Card key = {index} icon = {cardData.icon} name ={cardData.name} content={cardData.content} />
-        ))}
-      </div>
-   </>
-    
+            <div className="flex justify-evenly m-28 gap-32 items-center">
+                {cards.map((cardData, index) => (
+                    <Card
+                        key={index}
+                        icon={cardData.icon}
+                        name={cardData.name}
+                        content={cardData.content}
+                        href = {cardData.href}
+                    />
+                ))}
+            </div>
 
-)
+        </>
+    )
 }
--- components/Card.jsx
+++ components/Card.jsx
@@ -1,13 +1,17 @@
 import React from 'react'
 import Image from 'next/image';
+import Link from 'next/link';
 
-function Card({icon, name, content}) {
-  const bgColor = (name == "Hall da Fama"? "bg-yellow-400":"bg-white");  
+function Card({icon, name, content,href}) {
+  const bgColor = (name == "Hall da Fama"? "bg-yellow-400":"bg-white"); 
+  const linkcolor = (name =="Hall da Fama"? "bg-white" : "bg-yellow-400")
   return (
-    <div className={`${bgColor} rounded-xl shadow-lg flex items-center flex-col p-8 gap-5`}>
+    <div className={`${bgColor} rounded-xl shadow-lg flex items-center flex-col px-10 py-20 gap-7 max-w-96 text-center`}>
       <Image src={icon} objectFit="cover"/>
       <h2 className='text-xl font-semibold'>{name}</h2>
       <p>{content}</p>
+
+      <Link href= {href} className=''> Saiba Mais →</Link>
     </div>
   )
 }
